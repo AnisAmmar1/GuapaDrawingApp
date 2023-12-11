d190b6c2658bde0d080adb845c561f5529a51efc
anisammar1 <98645613+AnisAmmar1@users.noreply.github.com>
2023-12-11 14:41:07 +0100

    done

---
+++ Back/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/DrawingApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
+const drawingSchema = new mongoose.Schema({
+    dataURL: String,
+});
+
+const Drawing = mongoose.model('Drawing', drawingSchema);
+
+app.post('/save-drawing', async (req, res) => {
+    const { dataURL } = req.body;
+
+    try {
+        const newDrawing = new Drawing({ dataURL });
+        await newDrawing.save();
+        res.json({ message: 'Drawing saved successfully!' });
+
+        const base64Data = dataURL.replace(/^data:image\/png;base64,/, '');
+        const imagePath = path.join('C:\\Users\\A N I S\\Desktop', `drawing_${Date.now()}.png`);
+
+        fs.writeFileSync(imagePath, base64Data, 'base64');
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+app.get('/get-drawings', async (req, res) => {
+    try {
+        const drawings = await Drawing.find();
+        res.json(drawings);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FaPaintBrush, FaEraser, FaSave, FaTrash, FaCamera } from 'react-icons/fa';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [canvas, setCanvas] = useState(null);
+    const [context, setContext] = useState(null);
+    const [drawing, setDrawing] = useState(false);
+    const [color, setColor] = useState('#000000');
+    const [photo, setPhoto] = useState(null); 
+
+    useEffect(() => {
+        const canvasElement = document.getElementById('drawingCanvas');
+        setCanvas(canvasElement);
+
+        if (canvasElement) {
+            setContext(canvasElement.getContext('2d'));
+        }
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [canvas]);
+
+    const handleResize = () => {
+        if (canvas) {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        }
+    };
+
+    const saveDrawing = async () => {
+        if (canvas) {
+            const dataURL = canvas.toDataURL('image/png');
+    
+            try {
+                await axios.post('http://localhost:5000/save-drawing', { dataURL }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    maxContentLength: Infinity,
+                });
+            } catch (error) {
+                console.error('Error saving drawing:', error);
+            }
+        }
+    };
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/get-drawings');
+            const drawings = response.data;
+
+            context.clearRect(0, 0, canvas.width, canvas.height);
+
+            drawings.forEach((drawing) => {
+                const img = new Image();
+                img.src = drawing.dataURL;
+                img.onload = () => {
+                    context.drawImage(img, 0, 0);
+                };
+            });
+        } catch (error) {
+            console.error('Error fetching drawings:', error);
+        }
+    };
+
+    const clearCanvas = () => {
+        if (context && canvas) {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        }
+    };
+
+    const takePhoto = () => {
+        navigator.mediaDevices
+            .getUserMedia({ video: true })
+            .then((stream) => {
+                const video = document.createElement('video');
+                const photoContext = canvas.getContext('2d');
+
+                video.srcObject = stream;
+                video.onloadedmetadata = () => {
+                    video.play();
+
+                    // Capture the photo after a delay 2s
+                    setTimeout(() => {
+                        photoContext.drawImage(video, 0, 0, canvas.width, canvas.height);
+                        setPhoto(canvas.toDataURL('image/png'));
+                        video.srcObject.getTracks().forEach((track) => track.stop());
+                    }, 2000);
+                };
+            })
+            .catch((error) => {
+                console.error('Error accessing camera:', error);
+            });
+    };
+
+    return (
+        <div className="app-container">
+                     <div className="toolbar">
+                <button onClick={() => setColor('#000000')}>
+                    <FaPaintBrush />
+                </button>
+                <button onClick={() => setColor('#FFFFFF')}>
+                    <FaEraser />
+                </button>
+                <button onClick={saveDrawing}>
+                    <FaSave />
+                </button>
+                <button onClick={clearCanvas}>
+                    <FaTrash />
+                </button>
+                <button onClick={takePhoto}>
+                    <FaCamera />
+                </button>
+                <div className="color-palette">
+                    <button style={{ backgroundColor: '#000000' }} onClick={() => setColor('#000000')}></button>
+                    <button style={{ backgroundColor: '#FFFFFF' }} onClick={() => setColor('#FFFFFF')}></button>
+                    <button style={{ backgroundColor: '#FF0000' }} onClick={() => setColor('#FF0000')}></button>
+                    <button style={{ backgroundColor: '#0000FF' }} onClick={() => setColor('#0000FF')}></button>
+                    <button style={{ backgroundColor: '#FFFF00' }} onClick={() => setColor('#FFFF00')}></button>
+                </div>
+            </div>
+            <canvas
+                id="drawingCanvas"
+                onMouseDown={(e) => {
+                    if (canvas) {
+                        setDrawing(true);
+                        context.beginPath();
+                        context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+                    }
+                }}
+                onMouseUp={() => setDrawing(false)}
+                onMouseMove={(e) => {
+                    if (!drawing || !canvas || !context) return;
+
+                    context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+                    context.strokeStyle = color;
+                    context.lineWidth = 5;
+                    context.stroke();
+                    context.beginPath();
+                    context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+                }}
+            ></canvas>
+            {photo && (
+                <div className="photo-overlay">
+                    <img src={photo} alt="Captured Photo" />
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
